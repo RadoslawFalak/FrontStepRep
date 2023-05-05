7bfc229c696023fc92c031d2204511aa37f36537
RadoslawFalak <radoslaw.falak@gmail.com>
2023-05-05 17:49:00 +0000

    Zapis automatyczny!

--- pushScript.sh
+++ pushScript.sh
@@ -1,4 +1,4 @@
-cd /home/r4d3k/Pulpit/Zadania FrontStep/Level_5/NewLocalRep
-GIT_SSH_COMMAND="ssh -i /home/r4d3k/.ssh/id_rsa" git add .
-GIT_SSH_COMMAND="ssh -i /home/r4d3k/.ssh/id_rsa" git commit -m "Zapis automatyczny!"
-GIT_SSH_COMMAND="ssh -i /home/r4d3k/.ssh/id_rsa" git push origin main
+cd ~/home/r4d3k/Pulpit/Zadania FrontStep/Level_5/NewLocalRep
+GIT_SSH_COMMAND="ssh -i /home/r4d3k/.ssh/id_rsa.pud" git add .
+GIT_SSH_COMMAND="ssh -i /home/r4d3k/.ssh/id_rsa.pud" git commit -m "Zapis automatyczny!"
+GIT_SSH_COMMAND="ssh -i /home/r4d3k/.ssh/id_rsa.pud" git push origin main
